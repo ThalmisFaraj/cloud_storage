a67abc97e64af4398ea1f724e62ccd6506f1d5b0
ThalmisFaraj <thalmis93@gmail.com>
2024-12-08 11:07:37 +0530

    auth form state

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,9 +1,10 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import {
   Form,
   FormControl,
@@ -14,26 +15,41 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import Link from "next/link";
 
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-});
+// const formSchema = z.object({
+//   fullName: z.string().min(2).max(50),
+//   email: z.string().min(2).max(50),
+// });
 
 type FormType = "sign-in" | "sign-up";
+const authFormSchema = (formType: FormType) => {
+  return z.object({
+    fullName: formType === "sign-in" ? z.null() : z.string().min(2).max(30),
+    email: z.string().email(),
+  });
+};
 
 function AuthForm({ type }: { type: FormType }) {
-  // 1. Define your form.
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const formSchema = authFormSchema(type);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      fullName: "",
+      email: "",
     },
   });
 
-  // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
+    setIsLoading(true);
+
+    setTimeout(() => setIsLoading(false), 1000);
+
     console.log(values);
   }
   return (
@@ -43,23 +59,85 @@ function AuthForm({ type }: { type: FormType }) {
           <h1 className="form-title">
             {type === "sign-in" ? "Sign In" : "Sing Up"}
           </h1>
+          {type === "sign-in" && (
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="shad-form-item">
+                    <FormLabel className="shad-form-label">Full Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your full name"
+                        className="shad-input"
+                        {...field}
+                      />
+                    </FormControl>
+                  </div>
+                  {/* <FormDescription>
+                    This is your public display name.
+                  </FormDescription> */}
+                  <FormMessage className="shad-form-message" />
+                </FormItem>
+              )}
+            />
+          )}
+
           <FormField
             control={form.control}
-            name="username"
+            name="email"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
-                <FormMessage />
+                <div className="shad-form-item">
+                  <FormLabel className="shad-form-label">Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your email"
+                      className="shad-input"
+                      {...field}
+                    />
+                  </FormControl>
+                </div>
+                {/* <FormDescription>
+                    This is your public display name.
+                  </FormDescription> */}
+                <FormMessage className="shad-form-message" />
               </FormItem>
             )}
           />
-          <Button type="submit">Submit</Button>
+
+          <Button
+            type="submit"
+            className="form-submit-button"
+            disabled={isLoading}
+          >
+            {type === "sign-in" ? "Sign In" : "Sing Up"}
+            {isLoading && (
+              <Image
+                src="/assets/icons/loader.svg"
+                alt=""
+                width={24}
+                height={24}
+                className="ml-2 animate-spin"
+              />
+            )}
+          </Button>
+          {errorMessage && <p className="error-message">* {errorMessage}</p>}
+          <div className="body-2 flex justify-center">
+            <p className="text-light-100">
+              {" "}
+              {type === "sign-in"
+                ? "Dont have an account?"
+                : "ALreqady have an account?"}
+            </p>
+            <Link
+              href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+              className="ml-1 font-medium text-brand"
+            >
+              {type === "sign-in" ? "Sign-up" : "Sign-in"}
+            </Link>
+          </div>
         </form>
       </Form>
       {/* OTP verification */}
